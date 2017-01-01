50cf111dce5a99d8d0009fa4ccb0324329ea8e3a
Charanjit Singh <ckhabra@gmail.com>
2017-01-01 13:39:12 +0530

    Add another test

--- code/test_recursion.py
+++ code/test_recursion.py
@@ -7,3 +7,4 @@ def test_is_palindrome():
     assert is_palindrome('abba') is True
     assert is_palindrome('abbab') is False
     assert is_palindrome('kanak') is True
+    assert is_palindrome('123123') is False
