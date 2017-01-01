8c0278dafadd76ef5d8d912092772d1d7dc5f428
Charanjit Singh <ckhabra@gmail.com>
2017-01-01 13:27:23 +0530

    Add sample code

---
+++ code/test_recursion.py
@@ -0,0 +1,9 @@
+from recursion import is_palindrome
+
+
+def test_is_palindrome():
+    assert is_palindrome('') is True
+    assert is_palindrome('a') is True
+    assert is_palindrome('abba') is True
+    assert is_palindrome('abbab') is False
+    assert is_palindrome('kanak') is True
